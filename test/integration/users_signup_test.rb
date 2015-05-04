d34250b4c9a2f71d9ee9999aeb45908798250927
spectre6000 <spectre6000@gmail.com>
2015-05-04 09:12:00 -0600

    assignment

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,9 +10,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
 
     assert_template 'users/new'
+    assert_select 'div #error_explanation'
+    assert_select 'div .field_with_errors'
   end
 
-  test "successful signup" do
+  test "valid signup information" do
     get signup_path
 
     assert_difference 'User.count', 1 do
@@ -20,6 +22,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
 
     assert_template 'users/show'
+    assert_not flash.empty?
   end
 
 end
