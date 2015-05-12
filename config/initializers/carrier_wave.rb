70a4ac41f058ae7a31a3621e87fc94f06fcbb8e6
spectre6000 <spectre6000@gmail.com>
2015-05-11 18:39:31 -0600

    ch11 exercises

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAITCC4QPHR54WSTAA'],
-      :aws_secret_access_key => ENV['O8Mft]X8Fpz}']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['hartlsampleapp']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
