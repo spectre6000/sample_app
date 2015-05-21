a2304ae45539c359b71a8ffcfa016d6886edc199
spectre6000 <spectre6000@gmail.com>
2015-05-21 14:20:56 -0600

    Add user following

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -65,4 +65,14 @@ class UsersControllerTest < ActionController::TestCase
     assert_not @alt_user.reload.admin?
   end
 
+  test "should redirect follewing when not logged in" do 
+    get :following, id: @user
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do 
+    get :followers, id: @user
+    assert_redirected_to login_url
+  end
+
 end
